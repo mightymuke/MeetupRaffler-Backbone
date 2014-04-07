4642f225376499019d9cdb875e63675688e9e201
Marcus Bristol <marcus.bristol@rededge.co.nz>
2014-04-07 22:47:31 +1200

    Added 2 way binding for demo

--- app/js/index.js
+++ app/js/index.js
@@ -1,89 +1,22 @@
 var indexModel = new Backbone.Model({
+ 	isEditing: false,
 	title: 'Dashboard'
 });
 
-var IndexView = Backbone.View.extend({
- 	isEditing: false,
-
+var IndexView = Backbone.Epoxy.View.extend({
 	el: '#template',
-
-	initialize: function() {
-		this.model.on('change', function() {
-			this.render();
-		}, this);
-	},
-
+	bindings: "data-bind",
 	events: {
 		'click button': 'handleClick',
-		'change input': 'updateTitle'
 	},
-
 	handleClick: function() {
-		//if (this.isEditing) {
-			this.isEditing = !this.isEditing;
-			this.render();
-		//}
+		this.model.set('isEditing', !this.model.get('isEditing'));
+		this.render();
 	},
-
-	updateTitle: function() {
-		this.model.set('title', this.$el.find(input).val());
-	},
-
 	render: function() {
 		var template = $('#index').html();
 		this.$el.html(_.template(template, this.model.toJSON()));
+		this.applyBindings();
 		return this;
 	}
 })
-
-
-// var TitleView = Backbone.View.extend({
-// 	initialize: function() {
-// 		this.model.on('change', function() {
-// 			this.render();
-// 		}, this);
-// 	},
-// 	render: function() {
-// 		this.$el.html(this.model.get('title'));
-// 		return this;
-// 	}
-// });
-
-// var title = new Backbone.Model({title: 'Dashboard'});
-
-// var tv = new TitleView({
-// 	model: title,
-// 	el: '#page-title'
-// })
-
-// tv.render();
-
-// var TitleEditorView = Backbone.View.extend({
-// 	isEditing: false,
-// 	events: {
-// 		'click button': 'handleClick',
-// 		'change input': 'updateTitle'
-// 	},
-// 	handleClick: function() {
-// 		//if (this.isEditing) {
-// 			this.isEditing = !this.isEditing;
-// 			this.render();
-// 		//}
-// 	},
-// 	updateTitle: function() {
-// 		this.model.set('title', this.$el.find(input).val());
-// 	},
-// 	render: function() {
-// 		if (this.isEditing) {
-// 			this.$el.html('<button>Done</button><div id="title-edit"><input type="text" ng-model="title"></input></div>');
-// 		} else {
-// 			this.$el.html('<button>Edit</button>');
-// 		}
-// 		return this;
-// 	}
-// });
-
-// var tev = new TitleEditorView({
-// 	model: title
-// });
-// $('.index-edit').append(tev.render().el);
